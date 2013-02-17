3e9f5fc86f48aecb37c37f57a904a66cc0d568fb
Mark Sadegi <mark.sadegi@gmail.com>
2013-02-16 18:18:41 -0600

    more of the name silly tweeks

--- lib/coin_set.rb
+++ lib/coin_set.rb
@@ -45,15 +45,17 @@ end
 # # otherwise, no remainder
 # cs = [11, 9, 8, 5, 4, 1]
 # cs.reject! do |coin_to_reject|
-# puts("<@>" * 6)
-#  puts "in reject! block | coin_to_reject = #{coin_to_reject} in coins_set array -> #{cs}"
+#  puts("<@>" * 6)
+#  puts("<in reject!>")
+#  puts "coin_to_reject = #{coin_to_reject} in coins_set array -> #{cs}"
 #  cs.any? do |coin_to_test_against|
+#     puts("<in any?>")
 #    puts "coin_to_test_against = #{coin_to_test_against}"
 #    if coin_to_reject < coin_to_test_against
 #      puts "in any? block | coin_to_reject = #{coin_to_reject} is less than coin_to_test_against = #{coin_to_test_against}"
 #      coin_to_test_against % coin_to_reject == 0
 #      puts "coin_to_test_against % coin_to_reject == #{coin_to_test_against % coin_to_reject}"
-#      puts("<@>" * 6)
 #    end
 #  end
+#  puts("<@>" * 12)
 # end
